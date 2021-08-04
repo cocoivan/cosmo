f41c7d114b11d6a3faeed3f6da8c7c4953f01031
lvyifan3 <lvyifan3@jd.com>
2021-08-04 23:03:19 +0800

    create project

---
+++ src/test/java/com/cosmo/rpc/CosmoRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmo.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
